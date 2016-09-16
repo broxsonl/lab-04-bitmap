0bcc8b9e0083a6f054b2c8cbbcf8bbfb6bb961b4
Lee Broxson <broxsonl@gmail.com>
2016-09-15 21:01:14 -0700

    put the running stuff back in place.

--- bitmap-destruction-force/lib/bitmapFileHelper.js
+++ bitmap-destruction-force/lib/bitmapFileHelper.js
@@ -21,12 +21,12 @@ colorArray = buf.slice(54, 1078);
 
 var currentColor;
 
-for (var i = 0;i <= 1024;i += 4){
+for (var i = 0; i < 1024;i += 4){
   currentColor = colorArray.slice(i, i+4);
   console.log('B' + currentColor[0] + ' + ' + 'G' + currentColor[1] + ' + ' + 'R' + currentColor[2] + ' + ' + 'A' + currentColor[3]);
   currentColor[0] = 0;
-  currentColor[1] = 255;
-  currentColor[2] = 0;
+  currentColor[1] = 0;
+  currentColor[2] = 255;
   currentColor[3] = 0;
 }
 
