95ef9d9db959971fcfb4156214869dc00b9afa34
Lee Broxson <broxsonl@gmail.com>
2016-09-18 13:46:33 -0700

    Cleaned up Grayscale constructor and refactored unnecessary code.

--- bitmap-destruction-force/model/grayscale-constructor.js
+++ bitmap-destruction-force/model/grayscale-constructor.js
@@ -1,30 +1,23 @@
 'use strict';
 
-const fs = require('fs');
-
-module.exports = exports = {};
-//const bitmapFileHelper = require('../lib/bitmapFileHelper.js')
+module.exports = function grayScale(buf){
+  var duck = new GrayScale(buf);
+  duck.grayScale(buf);
+};
 
 const GrayScale = function(buf){
   this.filesize = buf.readUInt32LE(2);
   this.offset = buf.readUInt32LE(10);
   this.colorArray = buf.slice(54, 1078);
-}
+};
 
-
-GrayScale.prototype.grayScale = function(buf){
+GrayScale.prototype.grayScale = function(){
   var currentColor;
-    for (var i = 0;i < 1024;i += 4){
-      currentColor = this.colorArray.slice(i, i+4);
-      currentColor[0] = 0;
-      currentColor[1] = 0;
-      currentColor[2] = 255;
-      currentColor[3] = 0;
-    }
-}
-
-module.exports = function grayScale(buf){
-  var duck = new GrayScale(buf);
-  duck.grayScale(buf);
-
-}
+  for (var i = 0;i < 1024;i += 4){
+    currentColor = this.colorArray.slice(i, i+4);
+    currentColor[0] = 0;
+    currentColor[1] = 0;
+    currentColor[2] = 255;
+    currentColor[3] = 0;
+  }
+};
